66ef812cc6d17ae8a2b89d3ae26f456940566115
Ittirit Thipphinit <itchampclub@gmail.com>
2017-06-14 11:44:10 +0700

    Create bot.php

--- bot.php
+++ bot.php
@@ -19,9 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'Happy' => $text,
-				'type' => 'text',
-				'hi' => 'Happy'
+				'hi' => $text,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
