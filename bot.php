70f3e0b6d19b31c9cf3d8101c9a4607f4c874d2e
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 03:26:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,12 +18,12 @@ $url = 'https://api.mlab.com/api/1/databases/itbot/collections/linebot?apiKey='.
 $json = file_get_contents('https://api.mlab.com/api/1/databases/itbot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
-  if (strpos($_msg, 'สอ') !== false) {
+if (strpos($_msg, 'สอ') !== false) {
   if (strpos($_msg, 'สอ') !== false) {
     $x_tra = str_replace("สอ","", $_msg);
     $pieces = explode(":", $x_tra);
     $_question=str_replace("น","",$pieces[0]);
-    $_answer=str_replace("","",$pieces[1]);
+    $_answer=str_replace("?","",$pieces[1]);
     //Post New Data
     $newData = json_encode(
       array(
