4a4f559bdf09cb856a8a1bb42ec142f1327aa617
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-15 09:38:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'hi' => $text
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,4 +44,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK555";
+echo "OK";
